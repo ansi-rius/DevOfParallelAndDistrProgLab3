3ed2a02c3e609a78e7c6c3ff25e2037b3e75cbc6
Anemone <tttaina@mail.ru>
2019-11-07 20:34:57 +0300

    Scripted auto-commit on change (2019-11-07 20:34:57) by gitwatch.sh

--- src/main/java/ShowDelayFlights.java
+++ src/main/java/ShowDelayFlights.java
@@ -1,8 +1,26 @@
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+
+import java.util.Arrays;
 
 public class ShowDelayFlights {
     public static void main(String[] args) {
+        //инициализация приложения
         SparkConf conf = new SparkConf().setAppName("example");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        //загрузка данных
+        JavaRDD<String> distFile = sc.textFile("war-and-peace-1.txt");
+        //разбиение строки на слова
+        JavaRDD<String> splitted = distFile.flatMap(
+                s -> Arrays.stream(s.split(" ")).iterator()
+        );
+        //отображение слов в пару <Слово, 1>
+        JavaPairRDD<String, Long> wordsWithCount = splitted.mapToPair(
+                s -> new Tuple2<>(s,1)
+        );
     }
 }
 
