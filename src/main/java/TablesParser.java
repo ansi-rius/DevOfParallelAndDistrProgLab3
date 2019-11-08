7241dae6a90bc44e82dd8494493fd51e8744a567
Anemone <tttaina@mail.ru>
2019-11-09 01:21:38 +0300

    Scripted auto-commit on change (2019-11-09 01:21:38) by gitwatch.sh

--- src/main/java/TablesParser.java
+++ src/main/java/TablesParser.java
@@ -10,7 +10,8 @@ public class TablesParser {
                 dictionaryFile.mapToPair(Hadoop
                         s -> new Tuple2<>(Hadoop s,1l)
                 );*/
-        flights.mapToPair(s->new Tuple2<> ()) //s[11] - origin, 
+        flights.mapToPair(s->new Tuple2<> ()) //s[11] - origin, s[14] - dest
+            //
         return flights;
     }
 }
