ae0630fe4506dbb883b6a7407fd066e1723027e7
Anemone <tttaina@mail.ru>
2019-11-09 01:21:07 +0300

    Scripted auto-commit on change (2019-11-09 01:21:07) by gitwatch.sh

--- src/main/java/TablesParser.java
+++ src/main/java/TablesParser.java
@@ -1,7 +1,16 @@
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
+import scala.Tuple2;
 
 import java.util.Arrays;
 
 public class TablesParser {
-    
+    public static JavaPairRDD<Tuple2<String,String>, FlightKey> makeFlightPair(JavaRDD<String[]> flights) {
+        /*JavaPairRDD<String, Long> dictionary =
+                dictionaryFile.mapToPair(Hadoop
+                        s -> new Tuple2<>(Hadoop s,1l)
+                );*/
+        flights.mapToPair(s->new Tuple2<> ()) //s[11] - 
+        return flights;
+    }
 }
