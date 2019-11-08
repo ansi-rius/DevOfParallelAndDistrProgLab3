1903af89dd963409b4c53e07d15765cef1251842
Anemone <tttaina@mail.ru>
2019-11-09 01:02:01 +0300

    Scripted auto-commit on change (2019-11-09 01:02:01) by gitwatch.sh

--- src/main/java/FlightKey.java
+++ src/main/java/FlightKey.java
@@ -2,5 +2,17 @@ import java.io.Serializable;
 
 public class FlightKey implements Serializable {
     double delay;
-    int counter, 
+    int counter, late, canceled;
+
+    FlightKey(String del, String cancel){
+        if (cancel.equals("0.00")) { //если по нулям, то рейс не отменен
+            canceled = 0;
+            if (del.equals("")) {//если задержки не было, то 0
+                delay = 0;
+            } else {
+                delay = Double.parseDouble(del); //если не пусто, то есть значение, забираем его
+                
+            }
+        }
+    }
 }
