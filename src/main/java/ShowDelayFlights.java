8a317ebc616341757a56333ed436e63b2df1ee92
Anemone <tttaina@mail.ru>
2019-11-09 02:28:14 +0300

    Scripted auto-commit on change (2019-11-09 02:28:14) by gitwatch.sh

--- src/main/java/ShowDelayFlights.java
+++ src/main/java/ShowDelayFlights.java
@@ -48,7 +48,8 @@ public class ShowDelayFlights {
         //c помощью функции reduce или аналогичных расчитываем максимальное
         //время опоздания, процент опоздавших+отмененных рейсов
         JavaPairRDD<Tuple2<String, String>, FlightKey> reduceData = originDestDelayCancelledFlightTuple.reduceByKey(TablesParser.reduce);
-        //
+        //формируем строки для результата... res должен быть:
+        // name_origin, name_dest, maxDelay, %Of
     }
 }
 
