b69d529813587566d774fff1cf9c1075a84e8964
Anemone <tttaina@mail.ru>
2019-11-08 14:39:19 +0300

    Scripted auto-commit on change (2019-11-08 14:39:18) by gitwatch.sh

--- src/main/java/TablesParser.java
+++ src/main/java/TablesParser.java
@@ -1,3 +1,9 @@
+import org.apache.spark.api.java.JavaRDD;
+
 public class TablesParser {
-    
+    public static JavaRDD<String[]> splitAirportsTable(JavaRDD<String> table) {
+        //принимаем строку, надо разсплитить..
+        
+        return table;
+    }
 }
