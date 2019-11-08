2e1a3a0c5ae22cfd398bd597deecb82a945d1516
Anemone <tttaina@mail.ru>
2019-11-08 14:39:33 +0300

    Scripted auto-commit on change (2019-11-08 14:39:33) by gitwatch.sh

--- src/main/java/TablesParser.java
+++ src/main/java/TablesParser.java
@@ -3,6 +3,9 @@ import org.apache.spark.api.java.JavaRDD;
 public class TablesParser {
     public static JavaRDD<String[]> splitAirportsTable(JavaRDD<String> table) {
         //принимаем строку, надо разсплитить..
+        /*JavaPairRDD<String, Long> wordsWithCount = splitted.mapToPair(
+                s -> new Tuple2<>(s, 1l)
+        );*/
         
         return table;
     }
