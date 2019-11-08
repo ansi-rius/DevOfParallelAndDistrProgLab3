d1f8a5cc17e6952ec0e5f3d1ff03c38afaf4471b
Anemone <tttaina@mail.ru>
2019-11-08 14:28:27 +0300

    Scripted auto-commit on change (2019-11-08 14:28:27) by gitwatch.sh

--- src/main/java/ShowDelayFlights.java
+++ src/main/java/ShowDelayFlights.java
@@ -12,28 +12,7 @@ public class ShowDelayFlights {
         SparkConf conf = new SparkConf().setAppName("example");
         JavaSparkContext sc = new JavaSparkContext(conf);
         //загрузка данных
-        JavaRDD<String> distFile = sc.textFile("war-and-peace-1.txt");
-        //разбиение строки на слова
-        JavaRDD<String> splitted = distFile.flatMap(
-                s -> Arrays.stream(s.split(" ")).iterator()
-        );
-        //отображение слов в пару <Слово, 1>
-        JavaPairRDD<String, Long> wordsWithCount = splitted.mapToPair(
-                s -> new Tuple2<>(s,1|)
-        );
-        //считаем одинаковые слова
-        JavaPairRDD<String, Long> collectedWords = wordsWithCount.reduceByKey(
-                (a,b) -> a + b
-        );
-        //Загружаем словарь
-        JavaRDD<String> dictionaryFile = sc.textFile("words.txt");
-        JavaPairRDD<String, Long> dictionary = dictionaryFile.mapToPair(
-                s->new Tuple2<>(s,1|)
-        );
-        //производим операцию join со словарем
-        JavaPairRDD<String, Tuple2<Long, Long>> joinValue = dictionary.join(collectedWords);
-        //Печатаем результат
-        System.out.println("res="+joinValue.collect());
+        JavaRDD<String> flights = sc.textFile("/users/Anemone/");
     }
 }
 
